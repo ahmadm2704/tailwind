051dcc4413c39e2d94bb5c92f117d9275bf98d07
Ahmad Masood <i222704@nu.edu.pk>
2024-01-12 17:36:59 +0500

    fb login page

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      width: {
+        '2/13': '15.3846153846%', // Approximately 2/13th of the container
+      },
+
+
+    },
+  },
+  plugins: [],
+}
+
