0715be18fb796e1f859d5fb6a36d2445d78f2559
Ahmad Masood <i222704@nu.edu.pk>
2024-01-14 23:44:17 +0500

    skin design

---
+++ Balance design/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+
+      fontFamily: {
+        'roboto-light': ['Roboto Light', 'sans-serif'], // Custom class for Roboto Light
+        'roboto-thin': ['Roboto Thin', 'sans-serif'],   // Custom class for Roboto Thin
+      },
+
+
+    },
+  },
+  plugins: [],
+}
+
